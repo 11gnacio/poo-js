3bfe974e8c160922cd12147afdb6c8399cba862b
ignacio.D <ignaciodiaz@liceovvh.cl>
2026-03-12 11:16:24 -0300

    hola

--- static/js/bucles.js
+++ static/js/bucles.js
@@ -2,13 +2,45 @@ console.log("Activo.....");
 
 // 1. El contador en pantalla}
 
-function contadorPantalla() {
+function Contador() {
     let numeros = [];
-    const continer = document.getElementById("");
-    const resultado = document.getElementById("");
-    for(let i = 1; i <= 10; i++) {
+    const continer = document.getElementById(`resultadoContainer`);
+    const resultado = document.getElementById(`result1`);
+    for (let i = 1; i <= 10; i++) {
         numeros.push(i);
     }
     resultado.textContent = `Contador: ${numeros.join(" - ")}`;
     continer.classList.remove(`d-none`)
+}
+
+// 2. Lista de Asistencia Automática
+
+function lista() {
+    let curso = ["Ana", "Diego", "Sofía", "Matias"];
+    const container = document.getElementById(`resultadoContainer2`);
+    const resultado = document.getElementById(`result2`);
+    resultado.textContent = ""; // Limpiar contenedor
+    for (let i = 0; i < curso.length; i++) {
+        resultado.textContent += `${curso[i]},`
+    }
+    container.classList.remove('d-none')
+}
+
+// 3.  Buscador de Aprobados (Escala 1 a 7)
+
+function notas() {
+    let notas = [3.5, 6.2, 5.0, 2.8, 4.5, 7.0];
+    let contadorAprobados = 0;
+
+    const container = document.getElementById("resultadoContainer3");
+    const resulto = document.getElementById("result3");
+
+    for (let i = 0; i < notas.length; i++) {
+        if (notas[i] >= 4.0) {
+            contadorAprobados++;
+        }
+    }
+
+    resulto.textContent = "Total de alumnos aprobados: " + contadorAprobados;
+    container.classList.remove("d-none");
 }
--- static/js/trabajos_02.js
+++ static/js/trabajos_02.js
@@ -6,3 +6,47 @@ menuBtn.addEventListener("click", () => {
     navMenu.classList.toggle("active");
 });
 
+function uno() {
+    let ingreso = parseInt(prompt("Ingrese algun dato"));
+
+    if (ingreso >= 18) {
+        alert("Acceso concedido. Eres mayor de edad.")
+
+    } else if (ingreso < 18) {
+        alert("Acceso denegado. Eres menor de edad")
+    };
+};
+
+function dos() {
+    let nombre = prompt("Ingrese su nombre...");
+    let usuarios = [];
+    if (nombre !== "") {
+        usuarios.push(nombre);
+        alert(`Hola ${usuarios}`);
+    } else {
+        alert("Error: El nombre no puede estar en blanco...")
+    }
+
+}
+
+function tres() {
+    let productos = ["celular", "teclado", "mouse"];
+    let nuevoProducto = "celular";
+
+    if (productos.includes(nuevoProducto)) {
+        alert("El producto ya está en el inventario");
+    } else {
+        alert("El producto no está en el inventario");
+    }
+}
+
+let bodega = ["caja1", "caja2", "caja3", "caja4", "caja5", "caja6"];
+
+function cuatro() {
+    if (bodega.lenght <= 5) {
+        alert("Espacio disponible")
+    } else {
+        alert("Bodega llena, eliminando último ingreso")
+        bodega.pop();
+    }
+}
