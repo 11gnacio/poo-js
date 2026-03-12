6c55ac09d46399e71174abb466213c9c7387056f
ignacio.D <ignaciodiaz@liceovvh.cl>
2026-03-12 14:30:53 -0300

    reinicio

--- static/js/trabajos_02.js
+++ static/js/trabajos_02.js
@@ -50,3 +50,83 @@ function cuatro() {
         bodega.pop();
     }
 }
+
+function cinco() {
+    let colaEspera = ["error_404", "Juan", "Sofia"];
+    if (colaEspera[0] == "error_404") {
+        colaEspera.shift()
+    }
+    alert(`Cola de espera limpia: ${colaEspera.join(" - ")}`)
+}
+
+function ceis() {
+    let foro = ["User1", "User2"];
+    let rol = prompt("Ingresar rol:");
+    if (rol.toLowerCase === "admin") {
+        foro.unshift(rol);
+        alert(foro);
+    } else {
+        foro.push(rol)
+        alert(foro);
+    }
+}
+
+function ciete() {
+    let nota = parseFloat(prompt("Ingrese la nota"));
+    if (nota >= 6 && nota <= 7) {
+        alert("Excelente, eximido")
+    } else if (nota >= 4 && nota <= 5.9) {
+        alert("Aprobado vas a examen")
+    } else if (nota < 4 && nota >= 1) {
+        alert("Reprobado")
+    } else {
+        alert("Ingrese un valor válido")
+    }
+}
+
+function ocho() {
+    let invitados = ["Ana", "Luis", "Camila", "Diego"];
+    let personas = prompt("Ingrese su nombre: ");
+    if (invitados.includes(personas)) {
+        alert(`¡Bienvenido ${personas} Pasa a la fiesta!`)
+    } else {
+        alert("Lo siento, no estas en la lista de invitados")
+    }
+}
+
+function nueve() {
+    let estanteria = [["Manzanas", "Peras"], ["Leche", "Yogur"]];
+    let seccion = prompt("Ingresar una seccion(0-1)");
+    let productos = prompt("Ingresar producto solicitado");
+    if (seccion !== 0 || seccion !== 1) {
+        alert("No tenemos esta seccion")
+    }
+    else if (estanteria[seccion].includes(productos)) {
+        alert("Producto encontrado en el estante")
+    } else {
+        alert("No tenemos ese producto en esa sección")
+    }
+};
+
+let bitacora = [];
+function dies() {
+    let nombreVisita = prompt("Ingresa tu nombre: ")
+    let esVip = parseInt(prompt(`¿Eres VIP?\n0 = no\n1 = si`));
+    nombreVisita = nombreVisita.trim();
+
+    if (nombreVisita == "") {
+        alert("Error: el nombre no puede estar vacío.");
+    } else if (bitacora.includes(nombreVisita)) {
+        alert("Esa persona ya está en la lista.");
+    } else if (esVip == 1) {
+        bitacora.unshift(nombreVisita);
+        alert(`Agregado correctamente a la lista:\n${bitacora.join(", ")}\nPersonas en total:
+    ${bitacora.length}`);
+    } else if (esVip == 0) {
+        bitacora.push(nombreVisita);
+        alert(`Agregado correctamente a la lista:\n${bitacora.join(", ")}\nPersonas en total:
+        ${bitacora.length}`);
+    } else {
+        alert("Ingrese valores validos.");
+    }
+};
