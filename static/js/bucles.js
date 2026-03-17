52f54440c0562085fd87e1e39b49792a78ee3137
ignacio.D <ignaciodiaz@liceovvh.cl>
2026-03-17 15:34:40 +0000

    actualizado 17

--- static/js/bucles.js
+++ static/js/bucles.js
@@ -44,3 +44,77 @@ function notas() {
     resulto.textContent = "Total de alumnos aprobados: " + contadorAprobados;
     container.classList.remove("d-none");
 }
+
+function Inventario() {
+    let productos = ["Teclado", "Mouse", "Agotado", "Monitor", "Agotado", "Audífonos"];
+    const container = document.getElementById('resultadoContainer4');
+    const resultado = document.getElementById('result4');
+    let disponibles = [];
+    resultado.textContent = "";
+    for (let i = 0; i < productos.length; i++) {
+        if (productos[i] !== "Agotado") {
+            disponibles.push(productos[i]);
+        }
+    }
+    resultado.textContent = `productos disponibles: ${disponibles.join(" - ")}`
+    container.classList.remove('d-none')
+}
+
+function Sumando() {
+    let aportes = [1500, 2000, 500, 3000, 1000];
+    let totalRecaudado = 0;
+    const container = document.getElementById('resultadoContainer5');
+    const resultado = document.getElementById('result5');
+    for (let i = 0; i < aportes.length; i++) {
+        totalRecaudado += aportes[i];
+    }
+    resultado.textContent = `La colecta reunió un total de: ${totalRecaudado}`
+    container.classList.remove('d-none')
+}
+
+function Formateador() {
+    let asistentes = ["Carlos", "MARIA", "pedro", "LUCIA"];
+    let resultado = [];
+    const container = document.getElementById('resultadoContainer6')
+    const result = document.getElementById('result6');
+    for (let i = 0; i < asistentes.length; i++) {
+        if (i % 2 === 0) {
+            resultado.push(`${asistentes[i]} VIP`);
+        } else {
+            resultado.push(`${asistentes[i]} `);
+        }
+    }
+    result6.textContent = resultado.join(" - ");
+    container.classList.remove('d-none')
+};
+
+function Stock() {
+    let bodega = ["Lápiz", "Cuaderno", "Goma", "Cuaderno", "Regla", "Cuaderno"];
+    let articuloBuscado = prompt("Ingrese el articulo que busca: ");
+    let vecesEncontrado = 0;
+    const container = document.getElementById('resultadoContainer7');
+    const resultado = document.getElementById('result7');
+    for (let i = 0; i < bodega.length; i++) {
+        if (articuloBuscado == bodega[i]) {
+            vecesEncontrado++
+        }
+    }
+    resultado.textContent = `El artículo ${articuloBuscado} se encuentra ${vecesEncontrado} veces en la bodega`
+    container.classList.remove('d-none')
+}
+
+function generarParrafos() {
+    let temperaturas = [22, 24, 28, 35, 21, 38];
+
+    const container = document.getElementById(`container8`);
+    const result = document.getElementById(`result8`);
+    
+    for (i = 0; i < temperaturas.length; i++) {
+        if (temperaturas[i] >= 30 && temperaturas[i] < 50) {
+            result.innerHTML +=
+            `<p class="text-danger"> ¡ALERTA! Temperatura critica
+            de ${temperaturas[i]} grados.</p><br>`;
+            container.classList.remove(`d-none`);
+        }
+    }
+}
