be3b0e77d8e9ab2bf4945a2da03f623d4393c060
ignacio.D <ignaciodiaz@liceovvh.cl>
2026-03-18 18:07:06 +0000

    nuevo

--- static/js/funciones.js
+++ static/js/funciones.js
@@ -1,6 +1,43 @@
-console.log("js conectado...")
+console.log("js conectado...");
 
-function listarAsistencia() {
-    const input = document.getElementById("input1").value;
+let asistencia = [];
+
+function agregarLista(nombre) {
+    asistencia.push(nombre);
+    return asistencia.join(", ");
+}
+
+function Contador() {
+    const container = document.getElementById("container1");
+    const result = document.getElementById("result1");
+    const input = document.getElementById("input1");
+
+    let nombre = input.value.trim();
+
+    if (nombre === "") return; // evita agregar vacío
+
+    let resultado = agregarLista(nombre);
+    result.textContent = resultado;
+
+    input.value = "";
+    container.classList.remove("d-none");
+}
+
+// Ejercicio 2
+
+let pacientes = ["Carlos", "Maria", "Diego"];
+function agregarUrgencia(nombre) {
+  pacientes.unshift(nombre);
+  return pacientes.join(", ");
+};
+
+function ingresarUrgencia() {
+    const container = document.getElementById("container2");
+    const result = document.getElementById("result2");
+    const input = document.getElementById("input2");
     
+    if (input.value !== "") {
+        let nombre = input.value;
+        let resultado = agregarUrgencia(nombre); //Llama a la función guardando 
+    }
 }
