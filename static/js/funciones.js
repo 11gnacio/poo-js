6b2211ef180b313319006336536aeb08987b0e43
ignacio.D <ignaciodiaz@liceovvh.cl>
2026-03-19 02:30:20 +0000

    funciones_avances

--- static/js/funciones.js
+++ static/js/funciones.js
@@ -3,7 +3,9 @@ console.log("js conectado...");
 let asistencia = [];
 
 function agregarLista(nombre) {
+
     asistencia.push(nombre);
+
     return asistencia.join(", ");
 }
 
@@ -25,19 +27,47 @@ function Contador() {
 
 // Ejercicio 2
 
-let pacientes = ["Carlos", "Maria", "Diego"];
-function agregarUrgencia(nombre) {
-  pacientes.unshift(nombre);
-  return pacientes.join(", ");
-};
+
+let pacientes = ["Carlos", "María", "Diego"];
 
 function ingresarUrgencia() {
-    const container = document.getElementById("container2");
-    const result = document.getElementById("result2");
     const input = document.getElementById("input2");
-    
-    if (input.value !== "") {
-        let nombre = input.value;
-        let resultado = agregarUrgencia(nombre); //Llama a la función guardando 
+    const result = document.getElementById("result2");
+    const container = document.getElementById("container2");
+
+    let nombre = input.value.trim();
+
+    if (nombre !== "") {
+
+        pacientes.unshift(nombre);
+
+        result.textContent = "Próximos a atender: " + pacientes.join(", ");
+
+        container.classList.remove("d-none");
     }
+
+    input.value = "";
+}
+
+// Ejercicio 3
+
+let entregas = ["Pizza", "Sushi", "Hamburguesa", "Ensalada"];
+
+function delivery() {
+    const inputTres = document.getElementById("input3");
+    const resulDos  = document.getElementById("result3");
+    const containerTres = document.getElementById("container3");
+   
+    let accion = inputTres.value.trim().toLowerCase();
+
+    if (accion === "despachar") {
+    entregas.shift();
+} else if (accion === "cancelar") {
+    entregas.pop();
+}
+    resulDos.textContent = "Pedidos pendientes: " + entregas.join(", ");
+
+    containerTres.classList.remove("d-none");
+
+    inputTres.value = "";
 }
